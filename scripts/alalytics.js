26947fed56f287666063c1275967e3888e920a4b
Anton Petrov <anton.petrof.99@gmail.com>
2024-08-19 21:46:45 +0400

    Lets start from the begining

--- scripts/alalytics.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('a[href="#XJKDHSTQ"]').addEventListener('click', function() {
-    dataLayer.push({
-        'event': 'checkoutOpened',
-        'eventCategory': 'Fundraise Up',
-        'eventAction': 'Checkout Button Click',
-        'eventLabel': 'Opened Checkout'
-    });
-});
